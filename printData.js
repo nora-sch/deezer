6b171480d472c022b0040c417eead7381c69d8ae
nora-sch <nora.schumann.development@gmail.com>
2023-04-25 18:33:50 +0200

    display player and add listener to player button

--- printData.js
+++ printData.js
@@ -1,14 +1,52 @@
-let container = document.querySelector('.album-list');
-let listBox = document.createElement('ul');
-listBox.classList.add('title-list');
+let container = document.querySelector(".album-list");
+let listBox = document.createElement("ul");
+listBox.classList.add("list");
 container.append(listBox);
 
-data.forEach(album => {
-    // titles
-    listBox.insertAdjacentHTML('afterbegin', `<li><img src="${album.album.cover_medium} " alt="${album.album.title}">${album.title}</li>`)
-    console.log(album.title);
+data.forEach((album) => {
+  // titles
+  let audioSource = album.preview;
+  listBox.insertAdjacentHTML(
+    "afterbegin",
+    `<li>
+        <img src="${album.album.cover_medium} " alt="${album.album.title}">
+        ${album.title}
+        <div class='play-button' name='${album.preview}'>PLAY</div>
+        <audio controls class='play hidden' name='${album.preview}'">
+        <source src=" ${album.preview}" type="audio/mp3">
+        </audio>
+        <a href="${album.link}">
+            LINK
+        </a>
+    </li>`
+  );
+  console.log(album.title);
 
-    // images
-    console.log(album.album.cover_medium);
+  // images
+  console.log(album.album.cover_medium);
+
+  // tracks
+  console.log(album.preview);
+  console.log(album.link);
 });
 
+const playButtons = document.querySelectorAll(".play-button");
+let isPlayOn = false;
+playButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event);
+    let attribute = btn.getAttribute("name");
+    console.log(attribute);
+    let player = document.querySelector(`audio[name='${attribute}']`);
+    // see how to pause other players
+    if (!isPlayOn) {
+      player.play();
+      isPlayOn = true;
+      console.log("play");
+    } else {
+      player.pause();
+      isPlayOn = false;
+      console.log("pause");
+    }
+  });
+});

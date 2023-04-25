4110f09205f7c2e682c887c550f039a4b403b63f
nora-sch <nora.schumann.development@gmail.com>
2023-04-25 16:02:08 +0200

    display medium images and titles

--- printData.js
+++ printData.js
@@ -4,7 +4,11 @@ listBox.classList.add('title-list');
 container.append(listBox);
 
 data.forEach(album => {
-    listBox.insertAdjacentHTML('beforebegin', `<li>${album.title}</li>`)
+    // titles
+    listBox.insertAdjacentHTML('afterbegin', `<li><img src="${album.album.cover_medium} " alt="${album.album.title}">${album.title}</li>`)
     console.log(album.title);
+
+    // images
+    console.log(album.album.cover_medium);
 });
 
